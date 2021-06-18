f117842ffeb431753bf67bfdd1884197fb7e3d2c
roquegabrielperullini <72764997+roquegabrielperullini@users.noreply.github.com>
2021-06-18 14:24:40 -0300

    navbar card iTEMlISTCONTEINER

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
 
 //COMPONENTS
 import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
     <>
       <NavBar />
       <h2>Las ofertas de la semana</h2>
+      <div className="App-container">      
+      <ItemListContainer title={"Bienvenido a:"} subtitle={"SMART COMMERCE"}/>
+      </div>
     </>
+    
   );
 }
 
---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function CartWidget() {
+  return (
+    <>
+    <div className="App">
+      <Card border="dark">
+        <Card.Img     
+        alt="logo empresa"
+        src={logo} 
+        backgroundColor="black"
+        width="40"
+        height="40"
+        className="d-inline-block align-top"         
+        />
+      </Card>
+      </div>
+    </>
+  );
+}
+
+export default CartWidget;
+
+
+//<Navbar.Brand>
+  //        <img
+    //        alt=""
+      //      src={logo}
+        //    width="30"
+          //  height="30"
+            //className="d-inline-block align-top"
+   //       />{" "}
+     //     Smart Commerce
+       // </Navbar.Brand>
